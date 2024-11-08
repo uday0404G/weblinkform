29a967f802caddda6bc1efbc11851e7ede36cdc6
uday0404G <udaylashkari2@gmail.com>
2024-11-08 18:13:26 +0530

    SOME CHANGES IN CODE

--- pages/superbase.js
+++ pages/superbase.js
@@ -1,7 +1,12 @@
-
+// supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+let supabase;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+  supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+
+export { supabase };
