15a67911a5bcae4243c702286eb2e52dd0a7bfab
uday0404G <udaylashkari2@gmail.com>
2024-11-08 17:39:09 +0530

    SOME CHANGES IN CODE

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,13 @@
 import Image from "next/image";
-import localFont from "next/font/local";
+
+import Wishes from "@/Components/Wishes";
 
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export default function Home() {
   return (
     <div >
-   
+   <Wishes/>
     </div>
   );
 }
