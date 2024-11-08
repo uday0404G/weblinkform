9fa8d5a4df884d8aae969b45d2a11e8a4cfca4a7
uday0404G <udaylashkari2@gmail.com>
2024-11-08 18:08:25 +0530

    SOME CHANGES IN CODE

--- Components/Wishes.jsx
+++ Components/Wishes.jsx
@@ -1,9 +1,5 @@
-import { supabase } from "@/pages/superbade";
 import React, { useState } from "react";
-
-
-
-
+import { supabase } from '@/pages/superbase';
 
 const Wishes = () => {
   const [wish, setWish] = useState({
@@ -12,6 +8,7 @@ const Wishes = () => {
     Wishis: ''
   });
   const [status, setStatus] = useState('');
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setWish((prevState) => ({
@@ -25,12 +22,10 @@ const Wishes = () => {
 
     const { Name, Relation, Wishis } = wish;
 
-   
     const { data, error } = await supabase
       .from('users')
       .insert([{ name: Name, relation: Relation, wish: Wishis }]);
 
-   
     if (error) {
       console.error(error);
       setStatus('Error adding wish.');
@@ -41,38 +36,34 @@ const Wishes = () => {
         Relation: '',
         Wishis: ''
       });
-      
-
     }
   };
+
   return (
     <div className="w-full justify-center items-center">
-    
-
-    
-      <div className="flex md:w-1/2 w-full pt-10 mb-20 ">
-       <form className="px-4 pb-5 border-2 border-[#C8AF6B] rounded text-center w-full m-4" onSubmit={handleSubmit}>
+      <div className="flex md:w-1/2 w-full pt-10 mb-20">
+        <form className="px-4 pb-5 border-2 border-[#C8AF6B] rounded text-center w-full m-4" onSubmit={handleSubmit}>
           <h2 className="text-lg font-semibold">Want to add your wish</h2>
           <input
             type="text"
-            name="Name" 
-            value={wish.Name} 
-            onChange={handleChange} 
+            name="Name"
+            value={wish.Name}
+            onChange={handleChange}
             placeholder="Your Name"
             className="form-control my-2 p-2 border border-gray-300 rounded w-full"
           />
           <input
             type="text"
-            name="Relation" 
-            value={wish.Relation} 
-            onChange={handleChange} 
+            name="Relation"
+            value={wish.Relation}
+            onChange={handleChange}
             placeholder="Relation"
             className="form-control my-2 p-2 border border-gray-300 rounded w-full"
           />
           <textarea
-            name="Wishis" 
-            value={wish.Wishis} 
-            onChange={handleChange} 
+            name="Wishis"
+            value={wish.Wishis}
+            onChange={handleChange}
             placeholder="Your Message"
             className="form-control my-2 p-2 border border-gray-300 rounded w-full"
           ></textarea>
@@ -85,8 +76,8 @@ const Wishes = () => {
             </button>
           </div>
         </form>
-    </div>
-      {status && <p>{status}</p>} 
+      </div>
+      {status && <p>{status}</p>}
     </div>
   );
 };
--- pages/_app.js
+++ pages/_app.js
@@ -6,19 +6,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        {/* Preconnect to Google Fonts */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-
-        {/* Font Links */}
-        <link
-          href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400;700;900&family=Great+Vibes&family=Figtree:ital,wght@0,300..900;1,300..900&display=swap"
-          rel="stylesheet"
-        />
+       
       </Head>
       <Component {...pageProps} />
     </>
